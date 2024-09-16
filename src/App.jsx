d5eb1b258b1ec09f29370d033ee96e9619a64d06
rajputvishal1124 <vishalsingh2962003@gmail.com>
2024-09-16 11:16:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-[100vh] bg-red-100 flex items-center justify-center text-2xl'>App</div>
+  )
+}
+
+export default App
